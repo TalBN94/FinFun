ec6be44de6473650969a2431ad1462ab6f880e1e
TalBN94 <talbarnathan@gmail.com>
2025-01-06 01:07:16 +0200

    Example of using settings file

--- FinFunApp/FinFun/Controllers/TransactionController.cs
+++ FinFunApp/FinFun/Controllers/TransactionController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace FinFunApp.Controllers;
 
@@ -8,16 +9,19 @@ public class TransactionController : ControllerBase
 {
     private static readonly string[] Names = ["Krespi, Sibony, Barney"];
     private readonly ILogger<TransactionController> _logger;
+    private readonly IConfiguration _configuration;
 
-    public TransactionController(ILogger<TransactionController> logger)
+    public TransactionController(ILogger<TransactionController> logger, IConfiguration Configuration)
     {
         _logger = logger;
+        _configuration = Configuration;
     }
 
     [HttpGet("names")]
     public IEnumerable<string> OurNames()
     {
         _logger.LogInformation("Get was called");
+        _logger.LogInformation("{}", _configuration["customConfig"]);
         return Names;
     }
 }

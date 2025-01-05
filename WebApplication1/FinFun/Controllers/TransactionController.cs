5bfecce5b99787e7a5cd2e69de40bf06846fcc10
TalBN94 <talbarnathan@gmail.com>
2025-01-05 22:35:21 +0200

    test controller

---
+++ WebApplication1/FinFun/Controllers/TransactionController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TransactionController : ControllerBase
+{
+    private static readonly string[] Names = ["Krespi, Sibony, Barney"];
+    private readonly ILogger<TransactionController> _logger;
+
+    public TransactionController(ILogger<TransactionController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("names")]
+    public IEnumerable<string> OurNames()
+    {
+        _logger.LogInformation("Get was called");
+        return Names;
+    }
+}

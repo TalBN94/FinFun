846d4b300516c6a358b2a5164a5247575d8eefd7
Omer Krespi <omerkrespi.1@gmail.com>
2025-01-07 00:13:18 +0200

    add: assets, basic design

--- FinFunClient/src/main.jsx
+++ FinFunClient/src/main.jsx
@@ -3,17 +3,19 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./Pages/HomePage";
 import Expense from "./Pages/Expense";
-
+import SplashScreen from "./Pages/LogoScreen";
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   
   <StrictMode>
     <BrowserRouter>
+    <SplashScreen>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/expense" element={<Expense/>} />
       </Routes>
+     </SplashScreen>
     </BrowserRouter>
   </StrictMode>
 );
